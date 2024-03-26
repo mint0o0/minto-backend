de42327b66e94b83ea20b7a6e47057c9743673ab
easyhak <jooin2000@naver.com>
2024-03-26 21:41:04 +0900

    init

---
+++ src/main/java/com/example/mintobackend/MintoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mintobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintoBackendApplication.class, args);
+    }
+
+}
