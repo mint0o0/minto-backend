e5f7e847aaef85ab05c5d53f6dbead2fa35f96a7
jihak <zhzhk17@gmail.com>
2024-03-27 13:35:57 +0900

    Feat: 축제쪽 작업 - 축제 관련 mock 데이터 생성 - 축제 entity 생성 - get all festival api 완료

--- src/main/java/com/example/mintobackend/controller/FestivalController.java
+++ src/main/java/com/example/mintobackend/controller/FestivalController.java
@@ -1,19 +1,31 @@
 package com.example.mintobackend.controller;
 
+import com.example.mintobackend.entity.Festival;
+import com.example.mintobackend.service.FestivalService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/festival")
+@RequiredArgsConstructor
 public class FestivalController {
 
+    private final FestivalService festivalService;
     // for testing
     @GetMapping("/{id}")
     public String getFestival(@PathVariable Integer id){
         System.out.println(id);
         return id.toString();
+    }
+
+    @GetMapping
+    public List<Festival> getFestivals(){
+        return festivalService.getAllFestival();
     }
 }
---
+++ src/main/java/com/example/mintobackend/entity/Festival.java
@@ -0,0 +1,28 @@
+package com.example.mintobackend.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+@Document(collection = "festival")
+@Getter
+public class Festival {
+    @Id
+    private String id;
+    private String name;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private List<String> imageList;
+    private String location;
+    private List<HashMap<String, Object>> missions;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+}
