499ad4b1f6440cb0658854ec907523eae7c439ba
easyhak <jooin2000@naver.com>
2024-04-03 17:24:27 +0900

    Feat

--- src/main/java/com/example/mintobackend/entity/Member.java
+++ src/main/java/com/example/mintobackend/entity/Member.java
@@ -5,6 +5,9 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Document(collection = "member")
 @Getter
 @Setter
@@ -22,4 +25,7 @@ public class Member {
     // enum type
     private Authority authority;
 
+    // todo: 어떻게 할지 좀더 생각
+    // festivalId: {visitDate: 2020-10-15, mission: [1, 3, 2]}
+    private HashMap<String, Object> visitFestivals;
 }
