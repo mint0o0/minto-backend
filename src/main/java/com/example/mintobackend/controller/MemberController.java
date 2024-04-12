cae4e8c651144344d8dd0a45addc54d43b87d708
jihak <zhzhk17@gmail.com>
2024-04-12 20:04:04 +0900

    Feat: 사용자 축제 미션 완료

--- src/main/java/com/example/mintobackend/controller/MemberController.java
+++ src/main/java/com/example/mintobackend/controller/MemberController.java
@@ -2,18 +2,33 @@ package com.example.mintobackend.controller;
 
 import com.example.mintobackend.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
-    public ResponseEntity getVisitedFestivals(@AuthenticationPrincipal User user){
+    // get 방문한 축제
+    @GetMapping("/member/festival")
+    public ResponseEntity<HashMap<String, Object>> getVisitedFestivals(@AuthenticationPrincipal User user){
+
         return null;
     }
 
+    // post 미션 저장
+    @PostMapping("/mission/complete")
+    public ResponseEntity<HashMap<String, HashMap<String, Object>>> completeMission(@AuthenticationPrincipal User user, @RequestBody HashMap<String, Object> map){
+        var r = memberService.missionCompleted(user.getUsername(), map.get("festivalId").toString(), Integer.valueOf(map.get("missionIndex").toString()));
+        return new ResponseEntity<>(r, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/mintobackend/dto/MemberRequestDto.java
+++ src/main/java/com/example/mintobackend/dto/MemberRequestDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.HashMap;
 
 
 @Getter
