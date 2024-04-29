dc2f97b792be88be343bcb9b346b59731597be2d
easyhak <jooin2000@naver.com>
2024-04-29 19:36:08 +0900

    Feat: 사용자 방문 기록

--- src/main/java/com/example/mintobackend/controller/MemberController.java
+++ src/main/java/com/example/mintobackend/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.example.mintobackend.controller;
 
+import com.example.mintobackend.dto.VisitFestivalDto;
+import com.example.mintobackend.entity.Festival;
 import com.example.mintobackend.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +22,10 @@ public class MemberController {
     private final MemberService memberService;
 
     // get 방문한 축제
-    @GetMapping("/member/festival")
-    public ResponseEntity<HashMap<String, Object>> getVisitedFestivals(@AuthenticationPrincipal User user){
-
-        return null;
+    @GetMapping("/member/visit/festival")
+    public ResponseEntity<List<VisitFestivalDto>> getVisitedFestivals(@AuthenticationPrincipal User user){
+        // 필요한 정보: 축제에 대한 정보, 방문 날짜
+        return new ResponseEntity<>(memberService.getVisitedFestival(user.getUsername()), HttpStatus.OK) ;
     }
 
     // post 미션 저장
@@ -31,4 +34,6 @@ public class MemberController {
         var r = memberService.missionCompleted(user.getUsername(), map.get("festivalId").toString(), Integer.valueOf(map.get("missionIndex").toString()));
         return new ResponseEntity<>(r, HttpStatus.OK);
     }
+
+
 }
--- src/main/java/com/example/mintobackend/service/MemberService.java
+++ src/main/java/com/example/mintobackend/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.mintobackend.service;
 
-import com.example.mintobackend.entity.Member;
+import com.example.mintobackend.dto.VisitFestivalDto;
+import com.example.mintobackend.entity.Festival;
 import com.example.mintobackend.exception.CannotFindElementException;
 import com.example.mintobackend.repository.FestivalRepository;
 import com.example.mintobackend.repository.MemberRepository;
@@ -20,18 +21,31 @@ public class MemberService {
     private final FestivalRepository festivalRepository;
     // member 가 방문한 축제 확인 하기
     @Transactional(readOnly = true)
-    public HashMap<String, List<Integer>> getVisitedFestival(String walletAddress){
+    public List<VisitFestivalDto> getVisitedFestival(String walletAddress){
         var member = memberRepository.findByWalletAddress(walletAddress).orElseThrow(
-                ()-> new CannotFindElementException("member 가 없습니다.")
+                ()-> new CannotFindElementException("member가 없습니다.")
         );
-        return null;
+        List<VisitFestivalDto> visitFestivalDtoList = new ArrayList<>();
+
+        member.getVisitFestivals().keySet().forEach(festivalId -> {
+            var festival = festivalRepository.findById(festivalId).orElseThrow(()->new CannotFindElementException("축제가 없습니다."));
+            System.out.println(festivalId);
+            System.out.println(festival.getName());
+            System.out.println(festival);
+            Object visitDate = member.getVisitFestivals().get(festivalId).get("visitDate");
+            var visitFestival = new VisitFestivalDto(festival);
+            visitFestival.setVisitDate(visitDate);
+            visitFestivalDtoList.add(visitFestival);
+        });
+
+        return visitFestivalDtoList;
     }
     
     // member 미션 성공 -> 미션이 성공 등록 하면 자동으로 축제 기록하게 된다.
     @Transactional
     public  HashMap<String, HashMap<String, Object>> missionCompleted(String walletAddress, String festivalId, Integer missionIndex){
         var member = memberRepository.findByWalletAddress(walletAddress).orElseThrow(
-                ()-> new CannotFindElementException("member 가 없습니다.")
+                ()-> new CannotFindElementException("member가 없습니다.")
         );
         // 만약 이미 방문한 축제에 값이 존재 한다면
         if (member.getVisitFestivals().containsKey(festivalId)){
