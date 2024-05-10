bccac7f773bb790fb5a6ee3e69c336e76c8d4048
easyhak <jooin2000@naver.com>
2024-05-11 01:44:20 +0900

    Fix: api uri 변경

--- src/main/java/com/example/mintobackend/controller/AdminController.java
+++ src/main/java/com/example/mintobackend/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.mintobackend.controller;
 
 
+import com.example.mintobackend.entity.Festival;
 import com.example.mintobackend.service.FestivalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,5 +21,13 @@ public class AdminController {
     public ResponseEntity<Object> addFestival(@RequestBody HashMap<String,Object> festival) {
         return new ResponseEntity<>(festivalService.addFestival(festival), HttpStatus.CREATED);
     }
+    @PutMapping("/festival/{id}/nft")
+    public ResponseEntity<Festival> createNft(@PathVariable String id, @RequestBody Object nft){
+        return new ResponseEntity<>(festivalService.insertNft(id, nft), HttpStatus.OK);
+    }
 
+    @GetMapping("/nftList/{festivalId}")
+    public ResponseEntity<Object> getNftList(@PathVariable String festivalId){
+        return new ResponseEntity<>(festivalService.getNftList(festivalId), HttpStatus.OK);
+    }
 }
